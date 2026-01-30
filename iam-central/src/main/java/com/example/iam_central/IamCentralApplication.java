1db5aa6a387eae6fc30dea2ddbf4bb7f097ebfb8
hungnguyenuet <hungnguyenuet@gmail.com>
2026-01-30 22:38:01 +0700

    initialize IAM central project with Spring Boot and Maven setup

---
+++ iam-central/src/main/java/com/example/iam_central/IamCentralApplication.java
@@ -0,0 +1,13 @@
+package com.example.iam_central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamCentralApplication.class, args);
+	}
+
+}
